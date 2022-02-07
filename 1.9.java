9fb589778e8cbaa7748677498ab46e7a0d414f9c
Derrick Day <brlkid@outlook.com>
2022-02-07 10:57:04 -0500

    adding 1.9

---
+++ 1.9.java
@@ -0,0 +1,72 @@
+public class Test1
+{
+    public static void main(String[] args)
+    {
+      String firstName = "Maria";
+      String lastName = "Hernandez";
+      System.out.println(firstName);
+      System.out.println(lastName);
+    }
+ }
+
+ public class Test1
+{
+    public static void main(String[] args)
+    {
+      System.out.println("Roses are red ");
+      System.out.println("Violets are blue ");
+      System.out.println("Sugar is sweet ");
+      System.out.print("And so are you ");
+}
+
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        double bill = 89.23;
+        int people = 3; 
+        double tipAsDecimal = 0.20; 
+        double total = bill+bill*tipAsDecimal;
+        double perPerson = total/people;
+        System.out.println(perPerson);
+    }
+}
+
+public class Test1
+{
+    public static void main(String[] args)
+    {
+      int secs;
+      int hours; 
+      int minutes; 
+      int hours = 24; 
+      minutes = hours*60; 
+      secs = minutes*60; 
+      secs5 = secs*5;
+    }
+}
+
+public class Test1
+{
+    public static void main(String[] args)
+    {
+      int week 50; 
+      int total = 500; 
+      double temp = total/week;
+      double months = temp/4;
+      System.out.println(months);
+    }
+}
+
+public class Test1
+{
+    public static void main(String[] args)
+    {
+        String name = "Layla";
+        String age = "16";
+     System.out.println("Your name is "+name);
+     System.out.println("Your age is "+age); 
+        // Add your code here
+    }
+}
+
