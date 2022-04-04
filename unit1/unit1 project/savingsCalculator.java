2f9b13829061a4f9bfb5dae71d2acb38cc98253a
Derrick Day <brlkid@outlook.com>
2022-04-04 00:43:19 -0400

    uploaded more units

--- unit1/unit1 project/savingsCalculator.java
+++ unit1/unit1 project/savingsCalculator.java
@@ -5,6 +5,6 @@ public class savingsCalculator {
  int hours = 40; 
  double rent = 2562.46;
  double 
- string sources = "https://www.rentcafe.com/average-rent-market-trends/us/ca/los-angeles/, https://www.ziprecruiter.com/Salaries/Cyber-Security-Professional-Salary"
+ String sources = "https://www.rentcafe.com/average-rent-market-trends/us/ca/los-angeles/, https://www.ziprecruiter.com/Salaries/Cyber-Security-Professional-Salary";
 }
 }
